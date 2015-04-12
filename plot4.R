88c17dcb6eda0cf3acebcc5df41757c53c070bcc
kevinlimhk <kevinlimhk@gmail.com>
2015-04-12 23:45:37 +0800

    Added comments

--- plot4.R
+++ plot4.R
@@ -26,8 +26,11 @@ plot4 <- function() {
     
     # Plot 4
     # Set par parameters
+    # Set to 4 graphs
     par(mfrow=c(2,2))
+    # Set margins for the plots
     par(mar=c(4.1,3.5,2,0.8))
+    # Set the distance of axis values to axis lines
     par(mgp = c(3, 0.4, 0))
     
     # 1st Plot
@@ -35,6 +38,7 @@ plot4 <- function() {
          type="l",
          xlab = "", ylab ="",
          main = "", cex.lab=0.8, cex.axis=0.6, tck=-0.02)
+    # Adding text to y axis
     mtext("Global Active Power", side=2, line=1.8, cex=0.6)
     
     # 2nd Plot
@@ -42,6 +46,7 @@ plot4 <- function() {
          type="l",
          xlab = "", ylab = "",
          main = "", cex.lab=0.8, cex.axis=0.6, tck=-0.02)
+    # Adding texts to x and y axis
     mtext("Voltage", side=2, line=1.8, cex=0.6)
     mtext("datetime", side=1, line=1.6, cex=0.6)
     
@@ -50,11 +55,14 @@ plot4 <- function() {
          type="l",
          xlab = "", ylab = "",
          main = "", cex.lab=0.8, cex.axis=0.7, tck=-0.02)
+    # Adding text to y axis
     mtext("Energy sub metering", side=2, line=1.8, cex=0.6)
+    # Adding lines for the other variables
     lines(dataset$DateTime, dataset$Sub_metering_2,
           type="l", col="red")
     lines(dataset$DateTime, dataset$Sub_metering_3,
           type="l", col="blue")
+    # Adding legend
     legend("topright", cex=0.7,bty="n", y.intersp=0.8, x.intersp=0.2, lty=1, 
            col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
     
@@ -63,6 +71,7 @@ plot4 <- function() {
          type="l",
          xlab = "", ylab = "",
          main = "", cex.lab=0.8, cex.axis=0.6, tck=-0.02)
+    # Adding texts to x and y axis
     mtext("datetime", side=1, line=1.6, cex=0.6)
     mtext("Global_reactive_power", side=2, line=1.8, cex=0.6)
     
